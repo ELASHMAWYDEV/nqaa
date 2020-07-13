441c0751cbac84dc2cc32a3ea6ad9abfe67a176c
ELASHMAWY DEV <contact@elashmawydev.com>
2020-07-13 21:09:12 +0200

    config file updated

--- config.php
+++ config.php
@@ -17,13 +17,15 @@ ini_set("log_errors", TRUE);
 
 //Database
 define('HOST', 'localhost'); //Editable 
-define('DB_NAME', 'elaszeex_nqaa'); //Editable
-define('DB_USER', 'elaszeex_test'); //Editable
-define('DB_PASS', 'qwertyuiop1234$#'); //Editable
+define('DB_NAME', 'nqaa'); //Editable
+define('DB_USER', 'root'); //Editable
+define('DB_PASS', ''); //Editable
 
 define('ROOT_FOLDER', ''); //Editable => set the sub folder for the website {Leave Empty for Root /}
 
+#***** YOU MUST SET THE ROOT_FOLDER LIKE THIS => admin/    OR    nq/
+
 //url
 $secured = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on'? "https://" : "http://";
-define('ROOT_URL', $secured . $_SERVER['HTTP_HOST'] . ROOT_FOLDER .'/');
-define('PUBLIC_URL', $secured . $_SERVER['HTTP_HOST'] . '/public/');
+define('ROOT_URL', $secured . $_SERVER['HTTP_HOST'] . '/' . ROOT_FOLDER);
+define('PUBLIC_URL', $secured . $_SERVER['HTTP_HOST'] . '/' . ROOT_FOLDER . 'public/');
